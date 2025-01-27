504a7b31a739f67a8e86ead865e17cc8f072bcab
Coder06-dev <gawkinga06@gmail.com>
2025-01-27 15:30:27 +0000

    Add fibonacci implementation

--- homework/fibonacci/fibonacci.hpp
+++ homework/fibonacci/fibonacci.hpp
@@ -1,11 +1,26 @@
 #pragma once
 
 int fibonacci_iterative(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    int result = 0;
+    int j = 0;
+    int k = 1;
+    if (sequence == 0 || sequence == 1)
+        return sequence;
+    else {
+        for (int i = 2; i <= sequence; i++) {
+            result = j + k;
+            j = k;
+            k = result;
+        }
+    }
+    return result;
 }
 
 int fibonacci_recursive(int sequence) {
-    // TODO: Your implementation goes here
-    return 0;
+    if (sequence == 0 || sequence == 1)
+        return sequence;
+    else {
+        return (fibonacci_recursive(sequence - 1) + fibonacci_recursive(sequence - 2));
+    }
 }
+
