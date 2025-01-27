8bfe5441a0b1d3f09afdfac94962e038e42565c7
Coder06-dev <72879522+Coder06-dev@users.noreply.github.com>
2025-01-27 21:52:41 +0100

    Update calculate.hpp

--- homework/calculate/calculate.hpp
+++ homework/calculate/calculate.hpp
@@ -1,24 +1,22 @@
 #pragma once
-#pragma once
 #include <string>
 
-std::string calculate(const std::string &command, int first, int second) {
+std::string calculate(const std::string& command, int first, int second) {
+    std::string result = "";
 
-  std::string result = "";
+    if (command == "add")
+        result = std::to_string(first + second);
+    else if (command == "subtract")
+        result = std::to_string(first - second);
+    else if (command == "multiply")
+        result = std::to_string(first * second);
+    else if (command == "divide") {
+        if (second == 0)
+            result = "Division by 0";
+        else
+            result = std::to_string(first / second);
+    } else
+        result = "Invalid data";
 
-  if (command == "add")
-    result = std::to_string(first + second);
-  else if (command == "subtract")
-    result = std::to_string(first - second);
-  else if (command == "multiply")
-    result = std::to_string(first * second);
-  else if (command == "divide") {
-    if (second == 0)
-      result = "Division by 0";
-    else
-      result = std::to_string(first / second);
-  } else
-    result = "Invalid data";
-
-  return result;
+    return result;
 }
